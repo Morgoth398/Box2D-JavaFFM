42bf7a8b7b97afbb4f3fbbf82cf56866edf4871e
Morgoth398 <132191387+Morgoth398@users.noreply.github.com>
2026-05-04 14:43:56 +0200

    Regenerate Enums

--- src/main/java/volucris/bindings/box2d/enums/ShapeType.java
+++ src/main/java/volucris/bindings/box2d/enums/ShapeType.java
@@ -5,11 +5,29 @@ package volucris.bindings.box2d.enums;
 
 public final class ShapeType {
 
+    /**
+     * A circle with an offset
+     */
     public static final int CIRCLE_SHAPE = 0;
+    /**
+     * A capsule is an extruded circle
+     */
     public static final int CAPSULE_SHAPE = 1;
+    /**
+     * A line segment
+     */
     public static final int SEGMENT_SHAPE = 2;
+    /**
+     * A convex polygon
+     */
     public static final int POLYGON_SHAPE = 3;
+    /**
+     * A line segment owned by a chain shape
+     */
     public static final int CHAIN_SEGMENT_SHAPE = 4;
+    /**
+     * The number of shape types
+     */
     public static final int SHAPE_TYPE_COUNT = 5;
 
 }
