4c72fb211a3a526ba316f06322c485f54c512f23
Morgoth398 <132191387+Morgoth398@users.noreply.github.com>
2026-05-04 14:43:55 +0200

    Remove Old Sources

--- src/main/java/volucris/engine/physics/box2d/contactEvents/ContactHitHandler.java
+++
@@ -1,9 +0,0 @@
-package volucris.engine.physics.box2d.contactEvents;
-
-@FunctionalInterface
-public interface ContactHitHandler {
-	/**
-	 * Do not store a reference to the hit event. It will be reused internally.
-	 */
-	void contactHit(ContactHitEvent hitEvent);
-}
